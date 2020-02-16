a4260ff38d200f2533fa43f6f3d67f12d8790c0e
evertoncastro <evertoncastro.sp@gmail.com>
2020-02-15 21:52:00 -0300

    More unittests

--- src/services/dv.js
+++ src/services/dv.js
@@ -1,16 +1,14 @@
 
-function calcModule10(serial) {
-    let pCalc = this.productCalc(serial);
-    console.log(pCalc);
-    let fCalc = ((Math.ceil(pCalc / 10) * 10) - pCalc % 10).toString();
+function calcModule10(serial, factor) {
+    let product = productCalcModule10(serial, factor);
+    let fCalc = ((Math.ceil(product[0] / 10) * 10) - product[0]).toString();
     return fCalc;
 }
 
-function productCalcModule10(serial) {
+function productCalcModule10(serial, factor) {
     let productSum = 0;
-    let factor = 2;
     serial.split('').map(v => {
-        productSum += this.deepParser(Number(v)*factor);
+        productSum += deepParser(Number(v)*factor);
         factor = factor == 2 ? 1 : 2;
     });
     return [productSum, factor];
@@ -22,7 +20,7 @@ function deepParser(value){
     if(sum<=9){
         return sum;
     }
-    return this.deepParser(sum)
+    return deepParser(sum)
 }
 
 module.exports = {
--- test/test-services-dv.js
+++ test/test-services-dv.js
@@ -8,14 +8,22 @@ it('Deep parser should return a single character numeric value [1-9]', function(
 });
 
 it('Product calc should return the correct value', function(done) {
-    expect(dv.productCalcModule10('001905009')).to.equal([25, 1]);
+    expect(dv.productCalcModule10('001905009', 2)).to.deep.equal([25, 1]);
     done();
 });
 
-it('Calc Module 10 should return the correct value', function(done) {
-    //expect(DV.calcModule10('001905009')).to.equal('5');
-    //expect(DV.calcModule10('4014481606')).to.equal('9');
-    //expect(DV.calcModule10('001905009')).to.equal('5');
+it('Calc Module 10 should return the correct value for field 1', function(done) {
+    expect(dv.calcModule10('001905009', 2)).to.equal('5');
+    done();
+});
+
+it('Calc Module 10 should return the correct value for field 2', function(done) {
+    expect(dv.calcModule10('4014481606', 1)).to.equal('9');
+    done();
+});
+
+it('Calc Module 10 should return the correct value for field 3', function(done) {
+    expect(dv.calcModule10('0680935031', 1)).to.equal('4');
     done();
 });
 
