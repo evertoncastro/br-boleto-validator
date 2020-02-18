bb4fb0cff41370b2a0b256272e07672f82d0e844
evertoncastro <evertoncastro.sp@gmail.com>
2020-02-18 06:37:42 -0300

    refactoring files

--- src/services/modules.js
+++ src/services/modules.js
@@ -1,28 +1,12 @@
 
-function calcModule10(serial, factor) {
-    let product = productCalcModule10(serial, factor);
-    return ((Math.ceil(product[0] / 10) * 10) - product[0]).toString();
-}
-
-function productCalcModule10(serial, factor) {
-    // TODO: refactor
-    let productSum = 0;
-    serial.split('').map(v => {
-        productSum += deepParser(Number(v)*factor);
-        factor = factor == 2 ? 1 : 2;
-    });
-    return [productSum, factor];
-}
-
-function deepParser(value){
-    // Even that the value will never be higher than 18 
-    // this function will convert every number > 18 to a singe character
-    let sum = 0;
-    value.toString().split('').map(v => sum+=Number(v));
-    if(sum<=9){
-        return sum;
-    }
-    return deepParser(sum)
+function calcModule10(serial) {
+    serial = serial.split('').reverse();
+    const sum = serial.reduce((total, value, index) => {
+        let prod = Number(value) * (((index + 1) % 2) + 1);
+        prod = (prod > 9 ? Math.trunc(prod / 10) + (prod % 10) : prod);
+        return total + prod;
+    }, 0);
+    return ((Math.ceil(sum / 10) * 10) - sum).toString();
 }
 
 function bankModule11(barCode){
@@ -41,7 +25,5 @@ function bankModule11(barCode){
 
 module.exports = {
     calcModule10: calcModule10,
-    productCalcModule10: productCalcModule10,
-    deepParser: deepParser,
     bankModule11: bankModule11
 }
--- test/test-services-modules.js
+++ test/test-services-modules.js
@@ -1,37 +1,27 @@
 var expect  = require('chai').expect;
 var modules = require('./../src/services/modules');
 
-it('Deep parser should return a single character numeric value [1-9]', function(done){
-    expect(modules.deepParser(2229)).to.equal(6);
-    expect(modules.deepParser(18)).to.equal(9);
-    expect(modules.deepParser(9)).to.equal(9);
-    done();
-});
 
-it('Product calc should return the correct value', function(done) {
-    expect(modules.productCalcModule10('001905009', 2)).to.deep.equal([25, 1]);
-    done();
-});
+describe('module Services', () => {
+    it('Calc Module 10 should return the correct value for field 1', function(done) {
+        expect(modules.calcModule10('001905009')).to.equal('5');
+        done();
+    });
 
-it('Calc Module 10 should return the correct value for field 1', function(done) {
-    expect(modules.calcModule10('001905009', 2)).to.equal('5');
-    done();
-});
+    it('Calc Module 10 should return the correct value for field 2', function(done) {
+        expect(modules.calcModule10('4014481606')).to.equal('9');
+        done();
+    });
 
-it('Calc Module 10 should return the correct value for field 2', function(done) {
-    expect(modules.calcModule10('4014481606', 1)).to.equal('9');
-    done();
-});
+    it('Calc Module 10 should return the correct value for field 3', function(done) {
+        expect(modules.calcModule10('0680935031')).to.equal('4');
+        done();
+    });
 
-it('Calc Module 10 should return the correct value for field 3', function(done) {
-    expect(modules.calcModule10('0680935031', 1)).to.equal('4');
-    done();
+    it('Calc Module 11 should return the correct module', function(done) {
+        expect(modules.bankModule11('0019373700000001000500940144816060680935031')).to.equal('3');
+        done();
+    });
 });
-
-it('Calc Module 11 should return the correct module', function(done) {
-    expect(modules.bankModule11('0019373700000001000500940144816060680935031')).to.equal('3');
-    done();
-});
-
 
 
