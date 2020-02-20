87738a585e06e12a36e44181c11afd68ee12e254
evertoncastro <evertoncastro.sp@gmail.com>
2020-02-19 23:12:36 -0300

    working on mountTaxBilletInfo

---
+++ src/services/taxBillet.js
@@ -0,0 +1,32 @@
+let taxBilletLine = require('./taxBilletLine');
+let modules = require('./modules');
+let errors = require('./../errors');
+
+
+function taxBillet(line){
+    // TODO: more tests for calls
+    const fields = taxBilletLine.splitBilletLine(line);
+    taxBilletLine.checkTaxField1DV(fields.field1, fields.field1DV);
+    taxBilletLine.checkTaxField2DV(fields.field2, fields.field2DV);
+    taxBilletLine.checkTaxField3DV(fields.field3, fields.field3DV);
+    taxBilletLine.checkTaxField4DV(fields.field4, fields.field4DV);
+    return mountTaxBilletInfo(fields);
+}
+
+function mountTaxBilletInfo(lineInfo){
+    // TODO: more tests for calls
+    let tempBarCode = `${lineInfo.field1}${lineInfo.field2}${lineInfo.field3}${lineInfo.field4}`;
+    const firstPart = tempBarCode.slice(0, 3);
+    const currentDV = tempBarCode.slice(3, 4);
+    const secondPart = tempBarCode.slice(4)
+    if(modules.taxModule10(`${firstPart}${secondPart}`) !== currentDV) throw new errors.BusinessException('INVALID BAR CODE DV');
+    return {
+        barCode: `${firstPart}${currentDV}${secondPart}`,
+        validLine: true
+    }
+}
+
+module.exports = {
+    taxBillet: taxBillet,
+    mountTaxBilletInfo: mountTaxBilletInfo
+}
---
+++ test/test-tax-billet.js
@@ -0,0 +1,78 @@
+let chai  = require('chai');
+let spies = require('chai-spies');
+let taxBillet = require('./../src/services/taxBillet');
+let taxBilletLine = require('./../src/services/taxBilletLine')
+const errors = require('./../src/errors');
+
+chai.use(spies);
+const expect = chai.expect;
+const sandbox = chai.spy.sandbox();
+
+
+describe('taxBillet Services', () => {
+    beforeEach(() => {
+        sandbox.on(taxBilletLine, ['splitBilletLine']);
+    });
+
+    afterEach(() => {
+        sandbox.restore(); // restores original methods on `array`
+    });
+
+    // it('mountTaxBilletInfo should call info from each field/block', (done) => {
+    //     bankBillet.mountTaxBilletInfo({
+    //         field1: '001905009',
+    //         field1DV: '5',
+    //         field2: '4014481606',
+    //         field2DV: '9',
+    //         field3: '0680935031',
+    //         field3DV: '4',
+    //         field4: '3',
+    //         field5: '37370000000100'
+    //     });
+    //     expect(bankBilletLine.infoFromField1).to.have.been.called.with.exactly('001905009', '5');
+    //     expect(bankBilletLine.infoFromField2).to.have.been.called.with.exactly('4014481606', '9');
+    //     expect(bankBilletLine.infoFromField3).to.have.been.called.with.exactly('0680935031', '4');
+    //     expect(bankBilletLine.infoFromField5).to.have.been.called.with.exactly('37370000000100');
+    //     done();
+    // });
+
+    it('mountTaxBilletInfo should return a valid tax billet bar code', (done) => {
+        let barCode = taxBillet.mountTaxBilletInfo({
+            field1: '82650000000',
+            field1DV: '3',
+            field2: '52620097148',
+            field2DV: '3',
+            field3: '22020593391',
+            field3DV: '8',
+            field4: '41918120022',
+            field4DV: '3'
+        });
+        expect(barCode).to.be.deep.equal({
+            barCode: '82650000000526200971482202059339141918120022',
+            validLine: true
+        });
+        done();
+    });
+
+    it('mountBankLine should throw exception for an invalid tax billet bar code', (done) => {
+        expect(() => taxBillet.mountTaxBilletInfo({
+            field1: '82610000000',
+            field1DV: '3',
+            field2: '52620097148',
+            field2DV: '3',
+            field3: '22020593391',
+            field3DV: '8',
+            field4: '41918120022',
+            field4DV: '3'
+        })).to.throw(errors.BusinessException(), 'INVALID BAR CODE DV')
+        done();
+    });
+
+    // it('bankBillet function should call split billet line', (done) => {
+    //     const line = '00190500954014481606906809350314337370000000100'
+    //     bankBillet.bankBillet(line);
+    //     expect(bankBilletLine.splitBilletLine).to.have.been.called.with.exactly(line);
+    //     done();
+    // });
+
+});
